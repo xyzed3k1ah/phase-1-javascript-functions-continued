2bf3e854ab3b532020772e91e09a9f284f5c1653
Zedikiah <xyzed3k1ah@gmail.com>
2023-11-01 15:32:17 -0600

    javascript functions continued

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+  }
+
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  
